b95081203ee76173ad22d17f79d3b82f2e447459
gabcytn <cayetanogabriel03@gmail.com>
2025-02-24 23:52:52 +0800

    update: users model and migrations uuid

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,10 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid("id")->primary();
             $table->string('email')->unique();
+            $table->string('name');
             $table->timestamp('email_verified_at')->nullable();
+            $table->string("profile_img")->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
