f9aa4c1f8c6fd22d28811b483f9ec51d318b3bc3
gabcytn <cayetanogabriel03@gmail.com>
2025-02-25 00:47:13 +0800

    fix: default value of user role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid("id")->primary();
             $table->string('email')->unique();
-            $table->smallInteger("role_id");
+            $table->smallInteger("role_id")->default(1);
             $table->string('name');
             $table->timestamp('email_verified_at')->nullable();
             $table->string("profile_img")->nullable();
