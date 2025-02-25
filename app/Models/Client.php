90865d6ce69933a06bf9212fdb84962cbfcad90a
gabcytn <cayetanogabriel03@gmail.com>
2025-02-25 19:48:18 +0800

    hide password and use uuids

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Client extends Model
 {
+    use HasUuids;
     protected $fillable = [
         'bookkeeper_id',
         'email',
@@ -15,6 +17,17 @@ class Client extends Model
         'password',
         'profile_img',
     ];
+
+    protected $hidden = [
+        "password"
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            "password" => "hashed"
+        ];
+    }
 
     public function bookkeeper(): BelongsTo
     {
