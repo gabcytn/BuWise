f5244aa061bad94a9c8445344881b020fc625211
gabcytn <cayetanogabriel03@gmail.com>
2025-02-25 11:31:03 +0800

    create clients migration

---
+++ database/migrations/2025_02_25_031840_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->foreignUuid("bookkeeper_id")->references("id")->on("users")->cascadeOnDelete();
+            $table->string("email")->unique();
+            $table->string("client_type");
+            $table->string("name");
+            $table->string("password");
+            $table->string("profile_img");
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
