3dc6454f43e74c113e989bd0a9176aa107ddc169
gabcytn <cayetanogabriel03@gmail.com>
2025-03-04 09:19:04 +0800

    style: register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,22 +6,17 @@
     <title>BuWise</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     @vite('resources/css/auth/register.css')
-    @vite('resources/js/register.js')
 </head>
 <body>
-
     <div class="register-container">
         <div class="left-section">
             <img src="{{ asset('images/imgbg.jpg') }}" alt="BuWise" class="register-image">
         </div>
-
         <div class="right-section">
             <form method="POST" action="{{ route('register') }}">
                 @csrf
-
-                <h2>Welcome!</h2>
+                <h2>Welcome</h2>
                 <p>Simplifying and Automating Your Workflow</p>
-
                 <div class="input-group">
                     <label for="name">Name</label>
                     <div class="input-wrapper">
@@ -29,44 +24,30 @@
                         <input id="name" type="text" name="name" required autocomplete="name">
                     </div>
                 </div>
-                
-                <div class="input-group mt-4">
+                <div class="input-group">
                     <label for="email">Email</label>
                     <div class="input-wrapper">
                         <span class="icon"><i class="fas fa-envelope"></i></span>
                         <input id="email" type="email" name="email" required autocomplete="username">
                     </div>
                 </div>
-                
-                <div class="input-group mt-4">
+                <div class="input-group">
                     <label for="password">Password</label>
                     <div class="input-wrapper">
                         <span class="icon"><i class="fas fa-lock"></i></span>
-                        <input id="password" type="password" name="password" required autocomplete="new-password">
+                        <input id="password" type="password" name="password" required>
                     </div>
                 </div>
-                
-                <div class="input-group mt-4">
+                <div class="input-group">
                     <label for="password_confirmation">Confirm Password</label>
                     <div class="input-wrapper">
                         <span class="icon"><i class="fas fa-lock"></i></span>
-                        <input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="new-password">
+                        <input id="password_confirmation" type="password" name="password_confirmation" required>
                     </div>
                 </div>
-                
-
-                <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                        {{ __('Already registered?') }}
-                    </a>
-
-                    <x-primary-button class="ms-3">
-                        {{ __('Sign Up') }}
-                    </x-primary-button>
-                </div>
+                <button type="submit">{{ "Sign Up" }}</button>
             </form>
         </div>
     </div>
-
 </body>
 </html>
