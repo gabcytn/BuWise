d4c5f652b67a0c0c5f2dd8842ed23c0fd291ffe9
gabcytn <cayetanogabriel03@gmail.com>
2025-04-03 20:09:34 +0800

    refactor: fortify and /enable-2fa route
    remove confirm password on enabling of mfa & deny access to /enable-2fa if 2fa is already enabled

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\EnableMFA;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\StaffController;
-use App\Http\Middleware\EnableMFA;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,23 +15,25 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', EnableMFA::class])->name('dashboard');
 
-
-Route::middleware(['auth', EnableMFA::class])->group(function () {
+Route::middleware(['auth', 'verified', EnableMFA::class])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // client related routes
-    Route::resource("/clients", ClientController::class)
-        ->only(["index", "store", "edit", "update", "destroy"]);
+    Route::resource('/clients', ClientController::class)
+        ->only(['index', 'store', 'edit', 'update', 'destroy']);
 
     // staff related routes
-    Route::resource("/staff", StaffController::class)
-        ->only(["index", "store", "edit", "update", "destroy"]);
+    Route::resource('/staff', StaffController::class)
+        ->only(['index', 'store', 'edit', 'update', 'destroy']);
 
-    Route::get("/enable-2fa", function () {
-        return view("auth.enable-mfa");
-    })->name("mfa.enable")->withoutMiddleware(EnableMFA::class);
+    Route::get('/enable-2fa', function (Request $request) {
+        if ($request->user()->two_factor_confirmed_at && session('status') !== 'two-factor-authentication-confirmed') {
+            return to_route('dashboard');
+        }
+        return view('auth.enable-mfa');
+    })->name('mfa.enable')->withoutMiddleware(EnableMFA::class);
 });
 
 // require __DIR__ . '/auth.php';
