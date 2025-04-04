c5b48371aac7b4be2b8a83e1b8c1d4cc4c740695
gabcytn <cayetanogabriel03@gmail.com>
2025-04-04 15:01:05 +0800

    refactor: minor changes in login register styles and markup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,48 @@
 <x-root-layout>
-    @vite('resources/css/auth/login.css')
-    @vite('resources/js/login.js')
-    <div class="login-container">
-        <div class="left-section">
-            <img src="{{ asset('images/imgbg.jpg') }}" alt="BuWise" class="login-image">
-        </div>
+    @vite(['resources/css/auth/login.css', 'resources/js/login.js'])
+    <div class="left-section">
+        <img src="{{ asset('images/imgbg.jpg') }}" alt="BuWise" class="login-image">
+    </div>
 
-        <div class="right-section">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+    <div class="right-section">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-                <h2 id="title">Welcome Back!</h2>
-                <p id="subtitle">Simplifying and Automating Your Workflow</p>
+            <h2 id="title">Welcome Back!</h2>
+            <p id="subtitle">Simplifying and Automating Your Workflow</p>
 
-                {{-- email field --}}
-                <div class="input-wrapper">
-                    <label for="email">{{ 'Email' }}</label>
-                    <div class="input-box">
-                        <i class="fas fa-envelope"></i>
-                        <input id="email" type="email" name="email" value="{{ old('email') }}" required />
-                    </div>
-                    @error('email')
-                        <p class="error-message">{{ $message }}</p>
-                    @enderror
+            {{-- email field --}}
+            <div class="input-wrapper">
+                <label for="email">{{ 'Email' }}</label>
+                <div class="input-box">
+                    <i class="fas fa-envelope"></i>
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" required />
                 </div>
+                @error('email')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+            </div>
 
-                {{-- password field --}}
-                <div class="input-wrapper">
-                    <label for="password">{{ 'Password' }}</label>
-                    <div class="input-box">
-                        <i class="fas fa-lock lock-icon"></i>
-                        <input id="password" type="password" name="password" required />
-                        <i class="fas fa-eye" id="toggle-password"></i>
-                    </div>
-                    @error('password')
-                        <p class="error-message">{{ $message }}</p>
-                    @enderror
+            {{-- password field --}}
+            <div class="input-wrapper">
+                <label for="password">{{ 'Password' }}</label>
+                <div class="input-box">
+                    <i class="fas fa-lock lock-icon"></i>
+                    <input id="password" type="password" name="password" required />
+                    <i class="fas fa-eye" id="toggle-password"></i>
                 </div>
+                @error('password')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+            </div>
 
-                <div class="forgot-password-container">
-                    <a class="#" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                </div>
+            <div class="forgot-password-container">
+                <a class="#" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            </div>
 
-                <button type="submit">{{ __('Login') }}</button>
-            </form>
-        </div>
+            <button type="submit">{{ __('Login') }}</button>
+        </form>
     </div>
 </x-root-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,42 @@
 <x-root-layout>
     @vite('resources/css/auth/register.css')
-    <div class="register-container">
-        <div class="left-section">
-            <img src="{{ asset('images/imgbg.jpg') }}" alt="BuWise" class="register-image">
-        </div>
-        <div class="right-section">
-            <form method="POST" action="{{ route('register') }}">
-                @csrf
-                <h2 id="title">Welcome</h2>
-                <p id="subtitle">Simplifying and Automating Your Workflow</p>
-                <div class="input-group">
-                    <label for="name">Name</label>
-                    <div class="input-wrapper">
-                        <span class="icon"><i class="fas fa-user"></i></span>
-                        <input id="name" type="text" name="name" required autocomplete="name">
-                    </div>
+    <div class="left-section">
+        <img src="{{ asset('images/imgbg.jpg') }}" alt="BuWise" class="register-image">
+    </div>
+    <div class="right-section">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <h2 id="title">Welcome</h2>
+            <p id="subtitle">Simplifying and Automating Your Workflow</p>
+            <div class="input-group">
+                <label for="name">Name</label>
+                <div class="input-wrapper">
+                    <span class="icon"><i class="fas fa-user"></i></span>
+                    <input id="name" type="text" name="name" required autocomplete="name">
                 </div>
-                <div class="input-group">
-                    <label for="email">Email</label>
-                    <div class="input-wrapper">
-                        <span class="icon"><i class="fas fa-envelope"></i></span>
-                        <input id="email" type="email" name="email" required autocomplete="username">
-                    </div>
+            </div>
+            <div class="input-group">
+                <label for="email">Email</label>
+                <div class="input-wrapper">
+                    <span class="icon"><i class="fas fa-envelope"></i></span>
+                    <input id="email" type="email" name="email" required autocomplete="username">
                 </div>
-                <div class="input-group">
-                    <label for="password">Password</label>
-                    <div class="input-wrapper">
-                        <span class="icon"><i class="fas fa-lock"></i></span>
-                        <input id="password" type="password" name="password" required>
-                    </div>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <div class="input-wrapper">
+                    <span class="icon"><i class="fas fa-lock"></i></span>
+                    <input id="password" type="password" name="password" required>
                 </div>
-                <div class="input-group">
-                    <label for="password_confirmation">Confirm Password</label>
-                    <div class="input-wrapper">
-                        <span class="icon"><i class="fas fa-lock"></i></span>
-                        <input id="password_confirmation" type="password" name="password_confirmation" required>
-                    </div>
+            </div>
+            <div class="input-group">
+                <label for="password_confirmation">Confirm Password</label>
+                <div class="input-wrapper">
+                    <span class="icon"><i class="fas fa-lock"></i></span>
+                    <input id="password_confirmation" type="password" name="password_confirmation" required>
                 </div>
-                <button type="submit">{{ 'Sign Up' }}</button>
-            </form>
-        </div>
+            </div>
+            <button type="submit">{{ 'Sign Up' }}</button>
+        </form>
     </div>
 </x-root-layout>
