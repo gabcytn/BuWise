02dd4c537a6f5fe86650587d19b802c0086a0ed2
gabcytn <cayetanogabriel03@gmail.com>
2025-04-24 10:05:28 +0800

    feat: seeders for 'enum'-like tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            StatusSeeder::class,
+            TransactionTypeSeeder::class
         ]);
     }
 }
