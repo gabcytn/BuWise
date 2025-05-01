6a49a28e69d8d1f75020dde1baa877c68a5af365
gabcytn <cayetanogabriel03@gmail.com>
2025-05-01 19:01:57 +0800

    minor style changes #3

--- resources/views/journal-entries/index.blade.php
+++ resources/views/journal-entries/index.blade.php
@@ -2,8 +2,16 @@
     @php
         $headers = ['Journal ID', 'Client Name', 'Transaction Type', 'Amount', 'Date', 'Action'];
     @endphp
-    <div class="container" style="max-width: 1250px; width: 90%; margin: 0 auto;">
-        <h2 id="page-title" style="margin-top: 1.25rem;">Journal Entry</h2>
+
+    @vite(['resources/css/journal-entries/index.css', 'resources/js/journal-entries/index.js'])
+    <div class="container">
+        <form class="create-journal-form" action="{{ route('journal-entries.create') }}" method="GET">
+            <div class="first-texts">
+                <h2 id="page-title">Journal Entry</h2>
+                <p>Manage and access your clients' transactions</p>
+            </div>
+            <button type="submit">Create</button>
+        </form>
         <select class="select-clients"
             style="width: 100%; background-color: var(--clear-white); padding: 0.65rem; margin-top: 1rem; border: none; border-radius: 5px;">
             <option value="all">All Clients</option>
@@ -21,13 +29,6 @@
                 </option>
             @endforeach
         </select>
-        <form action="{{ route('journal-entries.create') }}" method="GET">
-            <button type="submit"
-                style="padding: 0.75rem 1.25rem; margin: 1rem 0; background-color: var(--green); border: none; border-radius: 0.25rem; color: var(--off-white); outline: none; cursor: pointer;">
-                Create
-            </button>
-        </form>
-        @vite('resources/js/journal-entries/index.js')
         @if (count($entries) > 0)
             <x-table-management :headers=$headers>
                 @foreach ($entries as $key => $entry)
@@ -58,7 +59,7 @@
                                     @csrf
                                     @method('DELETE')
                                     <button type="button"
-                                        style="background-color: transparent; border: none; outline: none;">
+                                        style="display: flex; background-color: transparent; border: none; outline: none;">
                                         <i class="fa-regular fa-trash-can" style="color: #ff0000; cursor: pointer"></i>
                                     </button>
                                 </form>
--- resources/views/journal-entries/show.blade.php
+++ resources/views/journal-entries/show.blade.php
@@ -2,13 +2,14 @@
     @php
         $headers = ['ID', 'Account Name', 'Account Group Name', 'Debit', 'Credit'];
     @endphp
-    <div class="container" style="max-width: 1250px; width: 90%; margin: 0 auto;">
-        <h2 style="margin-top: 1.25rem; margin-bottom: 0.5rem;">Ledger Entries</h2>
+    @vite('resources/css/journal-entries/show.css')
+    <div class="container">
+        <h2 id="page-title" style="margin-top: 1.25rem; margin-bottom: 0.5rem;">Ledger Entries</h2>
         <p>Description: {{ $description }}
         <p>
             <x-table-management :headers=$headers>
                 @foreach ($ledgerEntries as $key => $entry)
-                    <tr>
+                    <tr class="journal-row">
                         <td>{{ $entry->id }}</td>
                         <td>{{ $entry->account_name }}</td>
                         <td>{{ ucfirst($entry->account_group_name) }}</td>
@@ -17,6 +18,8 @@
                     </tr>
                 @endforeach
             </x-table-management>
-            <a href="{{ url()->previous() }}" style="text-decoration: none;">Back</a>
+        <form class="back-form" action="{{ route('journal-entries.index') }}" method="GET">
+            <button type="submit">Back</button>
+        </form>
     </div>
 </x-app-layout>
