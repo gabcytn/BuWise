9e58587ebf23a0f2ad1e944c32a6d81d9df6d1b5
gabcytn <cayetanogabriel03@gmail.com>
2025-05-23 22:51:28 +0800

    feat: change password api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MobileInvoiceController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Http\Controllers\TwoFactorSecretKeyController;
@@ -35,4 +36,26 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+});
+
+Route::put('/user/password', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'old_password' => 'required|string|min:8',
+        'new_password' => 'required|string|min:8',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+    if (!$user || !Hash::check($request->old_password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $user->password = $request->new_password;
+    $user->save();
+
+    return Response::json([
+        'message' => 'Successfully changed password'
+    ]);
 });
