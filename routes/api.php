9be9682d4937259aa13cb95a4ea98bea8ef3a84f
gabcytn <cayetanogabriel03@gmail.com>
2025-05-24 11:20:44 +0800

    refactor: move api closure functions to a separate controller class

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,13 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\MobileInvoiceController;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Http\Controllers\TwoFactorSecretKeyController;
 
-Route::post('/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-});
+Route::post('/login', [ApiAuthController::class, 'login']);
+Route::put('/user/password', [ApiAuthController::class, 'updatePassword']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/invoices', [MobileInvoiceController::class, 'store']);
@@ -36,26 +18,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-});
-
-Route::put('/user/password', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'old_password' => 'required|string|min:8',
-        'new_password' => 'required|string|min:8',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-    if (!$user || !Hash::check($request->old_password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    $user->password = $request->new_password;
-    $user->save();
-
-    return Response::json([
-        'message' => 'Successfully changed password'
-    ]);
 });
