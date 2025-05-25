3b6ce18ce7eaeca17990e34f03843ef1075e584c
gabcytn <cayetanogabriel03@gmail.com>
2025-05-25 20:14:22 +0800

    minor style changes #8

--- resources/js/journal-entries/index.js
+++ resources/js/journal-entries/index.js
@@ -4,10 +4,6 @@ const deleteEntryButtons = document.querySelectorAll(".action-column button");
 const confirmableDialog = document.querySelector(".confirmable-dialog");
 const confirmableDialogAffirmButton = document.querySelector(".affirm-button");
 const confirmableDialogDenyButton = document.querySelector(".deny-button");
-const selectType = document.querySelector(".select.select-type");
-const selectInvoice = document.querySelector(".select.select-invoice");
-
-displaySelect();
 
 confirmableDialogDenyButton.addEventListener("click", () => {
     confirmableDialog.close();
@@ -31,35 +27,4 @@ tr.forEach((row) => {
             location = row.dataset.url;
         }
     });
-});
-
-selectType.addEventListener("change", () => {
-    displaySelect();
-});
-
-function displaySelect() {
-    const selectedType = selectType.options[selectType.selectedIndex].value;
-    if (selectedType === "invoices") {
-        selectInvoice.disabled = false;
-        selectInvoice.required = true;
-        selectInvoice.classList.remove("d-none");
-    } else {
-        selectInvoice.disabled = true;
-        selectInvoice.required = false;
-        selectInvoice.classList.add("d-none");
-    }
-}
-
-const selectClients = document.querySelector(".select-clients");
-
-selectClients.addEventListener("change", () => {
-    const selectedClient =
-        selectClients.options[selectClients.selectedIndex].value;
-
-    if (selectedClient === "all") {
-        // location.href = location.origin + location.pathname;
-        return;
-    }
-    // location.href =
-    // location.origin + location.pathname + `?filter=${selectedClient}`;
 });
--- resources/views/journal-entries/index.blade.php
+++ resources/views/journal-entries/index.blade.php
@@ -17,94 +17,97 @@
     <div class="container">
         <form class="create-journal-form" action="{{ route('journal-entries.create') }}" method="GET">
             <div class="first-texts">
-                <h2 id="page-title">Journal Entry</h2>
-                <p>Manage and access your clients' transactions</p>
+                <h2 id="page-title">General Journal</h2>
+                <p>Create journal entries and organize financial records.</p>
             </div>
-            <button type="submit">Create</button>
+            <div class="button-container">
+                <button type="submit">Create</button>
+                <button type="button" id="dropdown-button">&#11206;</button>
+                <button type="button" id="vertical-ellipsis">&#8942;</button>
+            </div>
         </form>
-        <form class="select-container">
-            <select class="select select-type" name="type">
-                <option value="all" {{ request()->query('type') === 'all' ? 'selected' : '' }}>Journals & Invoices
-                </option>
-                <option value="journals" {{ request()->query('type') === 'journals' ? 'selected' : '' }}>
-                    From Journals Only
-                </option>
-                <option value="invoices" {{ request()->query('type') === 'invoices' ? 'selected' : '' }}>
-                    From Invoices Only
-                </option>
-            </select>
-            <select class="select select-invoice d-none" name="invoice">
-                <option value="all" {{ request()->query('invoice') === 'all' ? 'selected' : '' }}>All</option>
-                <option value="approved" {{ request()->query('invoice') === 'approved' ? 'selected' : '' }}>Approved
-                </option>
-                <option value="pending" {{ request()->query('invoice') === 'pending' ? 'selected' : '' }}>Pending
-                </option>
-                <option value="rejected" {{ request()->query('invoice') === 'rejected' ? 'selected' : '' }}>Rejected
-                </option>
-            </select>
-            <select class="select select-clients" name="client">
-                <option value="all" {{ request()->query('client') === 'all' ? 'selected' : '' }}>All Clients</option>
-                @foreach ($clients as $client)
-                    <option {{ request()->query('client') === $client->id ? 'selected' : '' }}
-                        value="{{ $client->id }}">
-                        {{ $client->name }}
-                    </option>
-                @endforeach
-            </select>
-            <button id="submit-filters">Run</button>
-        </form>
-        @if (count($entries) > 0)
-            <x-table-management :headers=$headers>
-                @foreach ($entries as $key => $entry)
-                    <tr class="journal-row" style="cursor: pointer;" data-url={{ "journal-entries/$entry->id" }}>
-                        <td>
-                            <p>{{ $entry->id }}</p>
-                        </td>
-                        <td>
-                            <p>{{ $entry->client_name }}</p>
-                        </td>
-                        <td>
-                            <p>{{ $entry->transaction_type }}</p>
-                        </td>
-                        <td>
-                            <p>{{ truncate($entry->description) }}</p>
-                        </td>
-                        <td>
-                            <p>&#8369;{{ number_format($entry->amount, 2) }}
-                        </td>
-                        <td>
-                            <p>{{ formatDate($entry->date) }}</p>
-                        </td>
-                        <td>
-                            <p>{{ $entry->creator }}</p>
-                        </td>
-                        <td>
-                            <strong class="{{ $entry->status }}">{{ ucfirst($entry->status) }}</strong>
-                        </td>
-                        <td class="action-column">
-                            <div>
-                                <a href="{{ route('journal-entries.edit', $entry->id) }}">
-                                    <i class="fa-regular fa-pen-to-square"></i>
-                                </a>
-                                <form action="{{ route('journal-entries.destroy', $entry->id) }}" method="POST"
-                                    id="{{ 'form-' . $entry->id }}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button"
-                                        style="display: flex; background-color: transparent; border: none; outline: none;">
-                                        <i class="fa-regular fa-trash-can" style="color: #ff0000; cursor: pointer"></i>
-                                    </button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </x-table-management>
-            @if ($errors->any())
-                <p style="color: red; font-size: 0.85rem;">{{ $errors->first() }}</p>
+        <div class="journal-container">
+            <form class="select-container" action="" method="GET">
+                <div class="select-container__left">
+                    <select class="select" name="period">
+                        <option value="all_time">All time</option>
+                        <option value="this_year">This year</option>
+                        <option value="last_year">Last year</option>
+                    </select>
+                    <select class="select select-clients" name="client">
+                        <option value="all" {{ request()->query('client') === 'all' ? 'selected' : '' }}>All Clients
+                        </option>
+                        @foreach ($clients as $client)
+                            <option {{ request()->query('client') === $client->id ? 'selected' : '' }}
+                                value="{{ $client->id }}">
+                                {{ $client->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="select-container__right">
+                    <div class="search-container">
+                        <input type="search" placeholder="Search" name="search" />
+                    </div>
+                    <div class="run-filter-container">
+                        <button id="submit-filters">Run</button>
+                    </div>
+                </div>
+            </form>
+            @if (count($entries) > 0)
+                <x-table-management :headers=$headers>
+                    @foreach ($entries as $key => $entry)
+                        <tr class="journal-row" style="cursor: pointer;" data-url={{ "journal-entries/$entry->id" }}>
+                            <td>
+                                <p>{{ $entry->id }}</p>
+                            </td>
+                            <td>
+                                <p>{{ $entry->client_name }}</p>
+                            </td>
+                            <td>
+                                <p>{{ $entry->transaction_type }}</p>
+                            </td>
+                            <td>
+                                <p>{{ truncate($entry->description) }}</p>
+                            </td>
+                            <td>
+                                <p>&#8369;{{ number_format($entry->amount, 2) }}
+                            </td>
+                            <td>
+                                <p>{{ formatDate($entry->date) }}</p>
+                            </td>
+                            <td>
+                                <p>{{ $entry->creator }}</p>
+                            </td>
+                            <td>
+                                <strong class="{{ $entry->status }}">{{ ucfirst($entry->status) }}</strong>
+                            </td>
+                            <td class="action-column">
+                                <div>
+                                    <a href="{{ route('journal-entries.edit', $entry->id) }}">
+                                        <i class="fa-regular fa-pen-to-square"></i>
+                                    </a>
+                                    <form action="{{ route('journal-entries.destroy', $entry->id) }}" method="POST"
+                                        id="{{ 'form-' . $entry->id }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button"
+                                            style="display: flex; background-color: transparent; border: none; outline: none;">
+                                            <i class="fa-regular fa-trash-can"
+                                                style="color: #ff0000; cursor: pointer"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </x-table-management>
+                @if ($errors->any())
+                    <p style="color: red; font-size: 0.85rem;">{{ $errors->first() }}</p>
+                @endif
+                {{ $entries->links() }}
             @endif
-            {{ $entries->links() }}
-        @endif
+        </div>
     </div>
 
     <x-confirmable-dialog title="Confirm Delete" affirmText="Delete" denyText="Back"></x-confirmable-dialog>
