49802a8b55e1e073e3ec8ca59e9cb8c3bae94b7a
gabcytn <cayetanogabriel03@gmail.com>
2025-06-07 12:05:19 +0800

    refactor: create separate reports controller for apis

---
+++ app/Http/Controllers/APIs/ReportsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\APIs;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\IncomeStatementController;
+use App\Models\AccountGroup;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+
+class ReportsController extends Controller
+{
+    public function incomeStatemetn(Request $request)
+    {
+        $request->validate([
+            'period' => 'required|in:this_year,this_month,this_week,today,last_week,last_month,last_year,all_time',
+        ]);
+        $clientId = $request->user()->id;
+        $period = new IncomeStatementController()->getStartAndEndDate($request->period);
+        $startDate = $period[0];
+        $endDate = $period[1];
+        $data = DB::table('ledger_entries AS le')
+            ->join('transactions AS tr', 'tr.id', '=', 'le.transaction_id')
+            ->join('users', 'users.id', '=', 'tr.client_id')
+            ->join('ledger_accounts AS acc', 'acc.id', '=', 'le.account_id')
+            ->where('tr.client_id', '=', $clientId)
+            ->whereIn('tr.status', ['approved', 'archived'])
+            ->whereBetween('tr.date', [$startDate, $endDate])
+            ->whereIn('acc.account_group_id', [AccountGroup::REVENUE, AccountGroup::EXPENSES])
+            ->groupBy('acc.id', 'acc.name', 'acc.account_group_id')
+            ->orderBy('acc.code')
+            ->select(
+                'acc.id AS acc_id',
+                'acc.name AS acc_name',
+                'acc.code AS acc_code',
+                'acc.account_group_id AS acc_group_id',
+                DB::raw("SUM(CASE WHEN le.entry_type = 'debit' THEN le.amount ELSE 0 END) AS debit"),
+                DB::raw("SUM(CASE WHEN le.entry_type = 'credit' THEN le.amount ELSE 0 END) AS credit")
+            )
+            ->get();
+
+        $revenues = [];
+        $expenses = [];
+        $revenuesTotal = 0;
+        $expensesTotal = 0;
+        foreach ($data as $datum) {
+            if ($datum->acc_code >= 400 && $datum->acc_code < 500) {
+                $revenues[] = $datum;
+                $revenuesTotal += $datum->debit > 0 ? -$datum->debit : $datum->credit;
+            } else {
+                $expenses[] = $datum;
+                $expensesTotal += $datum->debit > 0 ? $datum->debit : -$datum->credit;
+            }
+        }
+
+        return Response::json([
+            'revenues' => $revenues,
+            'expenses' => $expenses,
+            'revenuesTotal' => $revenuesTotal,
+            'expensesTotal' => $expensesTotal,
+            'net' => $revenuesTotal - $expensesTotal,
+        ]);
+    }
+}
