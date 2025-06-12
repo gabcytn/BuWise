5d2a2a3d5f5be643e056bbcf342b0669320a1831
EliiiBaltazar <heyitslyzab@gmail.com>
2025-06-13 06:25:16 +0800

    navigation

--- resources/js/components/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.nav-dropdown').forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const parent = this.closest('li');
-            parent.classList.toggle('open');
-        });
-    });
-});
