c7d2922242610c02fdf83d9ab49d6e17fef03f23
gabcytn <cayetanogabriel03@gmail.com>
2025-06-12 13:50:11 +0800

    feat: task policies

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Role;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TaskPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): Response
+    {
+        switch ($user->role_id) {
+            case Role::ACCOUNTANT:
+            case Role::LIAISON:
+            case Role::CLERK:
+                return Response::allow();
+                break;
+            default:
+                return Response::denyAsNotFound();
+                break;
+        }
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Task $task): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): Response
+    {
+        return $this->viewAny($user);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Task $task): Response
+    {
+        $validRole = in_array($user->role_id, [Role::ACCOUNTANT, Role::LIAISON, Role::CLERK]);
+        return $validRole && $task->assigned_to === $user->id
+            ? Response::allow()
+            : Response::denyAsNotFound();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Task $task): Response
+    {
+        return $this->update($user, $task);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Task $task): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Task $task): bool
+    {
+        return false;
+    }
+}
