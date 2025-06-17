1b4cfc9d986809bca3a71bed59132b0a8d7b7c9b
gabcytn <cayetanogabriel03@gmail.com>
2025-06-17 18:12:20 +0800

    feat: cash flow & profit loss data for api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\APIs\AuthController;
 use App\Http\Controllers\APIs\MobileInvoiceController;
 use App\Http\Controllers\APIs\ReportsController;
 use App\Http\Controllers\APIs\TasksController;
+use App\Http\Controllers\InsightsController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,6 +12,8 @@ Route::put('/user/password', [AuthController::class, 'updatePassword']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/client-tasks', [TasksController::class, 'index']);
+    Route::get('/cash-flow/{user?}', [InsightsController::class, 'cashFlow']);
+    Route::get('/profit-and-loss/{user?}', [InsightsController::class, 'profitAndLoss']);
 
     Route::resource('/invoices', MobileInvoiceController::class);
 
