69028fa458fcbf052682471c178a41483e122cba
gabcytn <cayetanogabriel03@gmail.com>
2025-06-20 11:37:46 +0800

    Revert "fix: use strings to replace models in notif"
    This reverts commit 6ba42b7b40a314b9dc2a15d9982dac41d4baf31b.

--- app/Notifications/NewTask.php
+++ app/Notifications/NewTask.php
@@ -4,7 +4,6 @@ namespace App\Notifications;
 
 use App\Models\Notification as ModelsNotification;
 use App\Models\Task;
-use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
@@ -20,8 +19,7 @@ class NewTask extends Notification
      * Create a new notification instance.
      */
     public function __construct(
-        private string $task_assigned_to_id,
-        private string $task_creator_id
+        private Task $task
     ) {
         //
     }
@@ -41,11 +39,10 @@ class NewTask extends Notification
      */
     public function toBroadcast(object $notifiable): BroadcastMessage
     {
-        $creator = User::find($this->task_creator_id);
         $n = ModelsNotification::create([
             'title' => 'New Task Assigned',
-            'user_id' => $this->task_assigned_to_id,
-            'description' => $creator->name . ' just assigned you a task',
+            'user_id' => $this->task->assigned_to,
+            'description' => $this->task->creator->name . ' just assigned you a task',
         ]);
         return new BroadcastMessage([
             'id' => $n->id,
