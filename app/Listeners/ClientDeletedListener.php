3545139ff0ce6157e8f1695c30e94d503f68dcd3
gabcytn <cayetanogabriel03@gmail.com>
2025-06-23 21:02:30 +0800

    fix: update clients cache when a client is deleted

---
+++ app/Listeners/ClientDeletedListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ClientDeleted;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Cache;
+
+class ClientDeletedListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ClientDeleted $event): void
+    {
+        $accountant_id = $event->accountant_id;
+        $accountant = User::find($accountant_id);
+
+        $clients = getClients($accountant);
+        Cache::put("$accountant_id-clients", $clients, 3600);
+    }
+}
