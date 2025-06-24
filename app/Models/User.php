3331cfb301a7bf61697d7e5d2c90d5685d9f26a8
gabcytn <cayetanogabriel03@gmail.com>
2025-06-24 13:16:23 +0800

    feat: failed invoices management

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -115,6 +116,11 @@ class User extends Authenticatable implements MustVerifyEmail
     public function transactions(): HasMany
     {
         return $this->hasMany(Transaction::class);
+    }
+
+    public function failedInvoices(): HasManyThrough
+    {
+        return $this->hasManyThrough(FailedInvoice::class, User::class, 'accountant_id', 'client_id');
     }
 
     /*
