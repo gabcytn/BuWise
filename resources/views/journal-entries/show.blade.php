ddbf93d1bbbf7e6be3fb62bb6a868509d6418a67
gabcytn <cayetanogabriel03@gmail.com>
2025-06-25 16:36:09 +0800

    style: journal show

--- resources/views/journal-entries/show.blade.php
+++ resources/views/journal-entries/show.blade.php
@@ -5,24 +5,8 @@
     @endphp
     @vite('resources/css/journal-entries/show.css')
     <div class="container">
-        <h2 id="page-title" style="margin-top: 1.25rem; margin-bottom: 0.5rem;">Ledger Entries</h2>
-        <p>Description: {{ $journalEntry->description }}</p>
-        @switch($journalStatus)
-            @case('approved')
-                <p>Approved</p>
-            @break
-
-            @case('pending')
-                <p>Approved</p>
-            @break
-
-            @case('rejected')
-                <p>Rejected</p>
-            @break
-
-            @default
-            @break
-        @endswitch
+        <h2 id="page-title">Ledger Entries</h2>
+        <p id="description">{{ $journalEntry->description }}</p>
         <x-table-management :headers=$headers>
             @foreach ($ledgerEntries as $entry)
                 <tr class="journal-row">
@@ -35,28 +19,27 @@
                 </tr>
             @endforeach
         </x-table-management>
-        @if (session('status'))
-            <p style="color: var(--green);">{{ session('status') }}</p>
-        @endif
-        @if ($journalStatus === 'pending' || $journalStatus === 'rejected')
-            <form action="{{ route('journal-entries.approve', $journalEntry) }}" method="POST">
-                @csrf
-                <button type="submit">Approve</button>
-            </form>
-        @endif
+        <div class="buttons-row">
+            <button type="button" id="back-button">Back</button>
+            @if ($journalStatus === 'pending' || $journalStatus === 'rejected')
+                <form action="{{ route('journal-entries.approve', $journalEntry) }}" method="POST">
+                    @csrf
+                    <button id="approve-button" type="submit">Approve</button>
+                </form>
+            @endif
 
-        @if ($journalStatus === 'pending' || $journalStatus === 'approved')
-            <form action="{{ route('journal-entries.reject', $journalEntry) }}" method="POST">
-                @csrf
-                <button type="submit">Reject</button>
-            </form>
-        @endif
-        <button type="button" id="back-button">Back</button>
-        @if ($journalEntry->type === 'journal')
-            <form action="{{ route('journal-entries.edit', $journalEntry) }}" method="GET">
-                <button type="submit">Edit</button>
-            </form>
-        @endif
+            @if ($journalStatus === 'pending' || $journalStatus === 'approved')
+                <form action="{{ route('journal-entries.reject', $journalEntry) }}" method="POST">
+                    @csrf
+                    <button id="reject-button" type="submit">Reject</button>
+                </form>
+            @endif
+            @if ($journalEntry->type === 'journal')
+                <form action="{{ route('journal-entries.edit', $journalEntry) }}" method="GET">
+                    <button id="edit-button" type="submit">Edit</button>
+                </form>
+            @endif
+        </div>
     </div>
     <script>
         document.querySelector("#back-button").addEventListener("click", () => {
