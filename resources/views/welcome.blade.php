e76444a7e906ea4fa32d7878284bff6af17b1aa3
EliiiBaltazar <heyitslyzab@gmail.com>
2025-06-27 00:21:18 +0800

    text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -152,10 +152,8 @@
         <p class="cta-heading">GET STARTED</p>
         <h2 class="cta-title">Be an Accountant</h2>
         <p class="cta-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-        </p>
+            Create your account and instantly step into your role as an accountant. Our platform empowers you to form your organization and manage your entire accounting firm with unparalleled ease. Beyond effortlessly adding clients and setting up staff accounts, you'll conquer all your key bookkeeping tasks in one centralized, intuitive platform. What's more, our system automates repetitive tasks like data entry and reconciliation, freeing up valuable time. We also provide seamless data migration for your Excel files, ensuring a smooth transition. And with robust security measures built-in, your sensitive financial data is always protected. </p>
               <a href="/register" class="cta-button">Sign up as an Accountant <span class="arrow">→</span>
-            
         </a>
     </div>
 </section>
