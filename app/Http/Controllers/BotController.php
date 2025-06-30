55583ba6fa09b91a97a65e83c746034a629567f1
gabcytn <cayetanogabriel03@gmail.com>
2025-06-30 22:07:04 +0800

    chore: remove unused variables

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class BotController extends Controller
@@ -15,10 +14,8 @@ class BotController extends Controller
         if ($user->role_id !== Role::BOT)
             abort(404);
         $clients = User::where('role_id', '=', Role::CLIENT)->get();
-        $accountants = User::where('role_id', '=', Role::ACCOUNTANT)->get();
         return view('invoices.bot-create', [
             'clients' => $clients,
-            'accountants' => $accountants,
         ]);
     }
 }
